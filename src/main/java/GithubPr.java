069768454e4808a58d517b333ca350b36e8dd16a
greek-zzf <1531037123@qq.com>
2021-03-17 17:19:47 +0800

    Optimize code and add SDK

--- src/main/java/GithubPr.java
+++ src/main/java/GithubPr.java
@@ -10,6 +10,10 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -37,16 +41,21 @@ public class GithubPr {
         }
     }
 
+    private static List<GitHubPullRequest> gitHubPullRequests;
+    private static final CloseableHttpClient httpclient = HttpClients.createDefault();
+    private static final String GITHUB_API = "https://api.github.com/repos/";
+    private static final String GITHUB_URL = "https://github.com/";
+
+
     public static void main(String[] args) throws IOException {
-        List<GitHubPullRequest> gitHubPullRequests = getFirstPageOfPullRequestsByHtml("gradle/gradle");
+        List<GitHubPullRequest> gitHubPullRequests = getFirstPageOfPullRequestsBySDK("gradle/gradle");
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle" 返回Pull request信息
     public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
-        List<GitHubPullRequest> gitHubPullRequests = new ArrayList<>();
+        gitHubPullRequests = new ArrayList<>();
 
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls");
+        HttpGet httpGet = new HttpGet(GITHUB_API + repo + "/pulls");
         httpGet.setHeader("Accept", "application/vnd.github.v3+json");
         CloseableHttpResponse response = httpclient.execute(httpGet);
 
@@ -58,13 +67,10 @@ public class GithubPr {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode node = objectMapper.readTree(result);
 
-            int number;
-            String title;
-            String author;
             for (int i = 0; i < node.size(); i++) {
-                number = node.get(i).get("number").asInt();
-                title = node.get(i).get("title").asText();
-                author = node.get(i).get("user").get("login").asText();
+                int number = node.get(i).get("number").asInt();
+                String title = node.get(i).get("title").asText();
+                String author = node.get(i).get("user").get("login").asText();
 
                 gitHubPullRequests.add(new GitHubPullRequest(number, title, author));
             }
@@ -77,10 +83,9 @@ public class GithubPr {
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle" 返回Pull request信息
     public static List<GitHubPullRequest> getFirstPageOfPullRequestsByHtml(String repo) throws IOException {
-        List<GitHubPullRequest> gitHubPullRequests = new ArrayList<>();
+        gitHubPullRequests = new ArrayList<>();
 
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
+        HttpGet httpGet = new HttpGet(GITHUB_URL + repo + "/pulls");
         CloseableHttpResponse response = httpclient.execute(httpGet);
 
         try {
@@ -91,20 +96,34 @@ public class GithubPr {
             Document doc = Jsoup.parse(result);
             ArrayList<Element> elements = doc.select(".js-issue-row");
 
-            int number;
-            String title;
-            String author;
             for (Element e : elements) {
-                title = e.child(0).child(1).child(0).text();
-                author = e.child(0).child(1).child(3).child(0).child(1).text();
+                String title = e.child(0).child(1).child(0).text();
+                String author = e.child(0).child(1).child(3).child(0).child(1).text();
                 String[] str = e.child(0).child(1).child(3).child(0).text().split(" ");
-                number = Integer.parseInt(str[0].substring(1));
+                int number = Integer.parseInt(str[0].substring(1));
                 gitHubPullRequests.add(new GitHubPullRequest(number, title, author));
             }
             EntityUtils.consume(responseEntity);
         } finally {
             response.close();
         }
+        return gitHubPullRequests;
+    }
+
+
+    public static List<GitHubPullRequest> getFirstPageOfPullRequestsBySDK(String repo) throws IOException {
+        gitHubPullRequests = new ArrayList<>();
+        // 使用匿名链接，访问到 GitHub
+        GitHub github = GitHub.connectAnonymously();
+        // 获取对应的仓库信息
+        GHRepository repository = github.getRepository(repo);
+        // 返会所有状态为 OPEN 的 PR 信息
+        List<GHPullRequest> pullRequests = repository.getPullRequests(GHIssueState.OPEN);
+
+        for (GHPullRequest gh : pullRequests) {
+            gitHubPullRequests.add(new GitHubPullRequest(gh.getNumber(), gh.getTitle(), gh.getUser().getLogin()));
+        }
+
         return gitHubPullRequests;
     }
 
